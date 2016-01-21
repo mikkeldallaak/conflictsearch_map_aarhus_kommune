e0cdecb1901bfc490196aad72036fbace23d0844
unknown <aztst45@WRK2016241.adm.aarhuskommune.dk>
2016-01-21 13:19:20 +0100

    first version

---
+++ aarhus-konflikt-driver.js
@@ -0,0 +1,38 @@
+(function() {
+    if (typeof (SeptimaD) == 'undefined') {
+        SeptimaD = {};
+        var checkReady = function(callback) {
+            if (window.jQuery) {
+                callback();
+            } else {
+                window.setTimeout(function() {
+                    checkReady(callback);
+                }, 100);
+            }
+        };
+        checkReady(function() {
+            
+			var scriptlocation = 'http://widget.cdn.septima.dk/1.10.30/';
+
+            var lib = decodeURIComponent((new RegExp('[?|&]widget-debug=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||'widget_.js';
+
+            jQuery.ajax({
+                url: scriptlocation + lib,
+                dataType: "script",
+                cache: true,
+                success: function() {
+                    SeptimaLoad.scriptlocation = scriptlocation;
+                    jQuery('[data-widget]').each(function () {
+
+                        SeptimaLoad.init({
+                            id: jQuery(this).attr('data-widget'),
+                            url: '' + jQuery(this).attr('data-widget') + '.json',
+                            dataType: 'json'
+                        });
+
+                    });
+                }
+            });
+        });
+    }
+})();
